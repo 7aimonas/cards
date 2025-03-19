982524d5daeafa0e8a9b9bed95c5bf977bb78496
Laimonas <laimonaszakas@gmail.com>
2025-03-19 21:47:39 +0100

    modal image test

--- src/ModalInfo.js
+++ src/ModalInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const modalImageUrl = "https://lh3.googleusercontent.com/pw/AP1GczOzALdd-8GuXdsUC4uzx6RVxMaHI92M0GPGljZ31pTCmRjOED1_sE06K1si4_nspyIlPzpUX0xw84tm5YyO_Xo87xckwFVbyL4pfT34zZu0n0jKOCebf-1rYkzKL4IMJW2i9GYABl1sF4mbmp6kGWne=w1773-h889-s-no-gm?authuser=0";
+const modalImageUrl2 = "https://photos.google.com/share/AF1QipNxwgDTKXzsdBepAM7VrJJBTaxdEFMvkktGLL17BikL86h7PayRu5_TPENRMilByQ/photo/AF1QipOyjIiY6yATq1xesXdVEDbnwkdFlFzScILUZCNK?key=SXZnQ2xzU05rQUNDd0Vza1Njd0xEY0pGYndGV0Nn";
 function InfoOverlay({ onClose, countriesAmount, coinsAmount }) {
     return (
       <div className="overlay">
@@ -14,7 +15,9 @@ function InfoOverlay({ onClose, countriesAmount, coinsAmount }) {
             
           </div>
           <img src={modalImageUrl} alt="Full Image" className="modal-image" />
-          <br></br> test image load
+          <br></br> test image load unedited pic below:
+          <br></br>
+          <img src={modalImageUrl2} alt="Full Image 2" className="modal-image" />
           <br></br>
           <button className="close-button" onClick={onClose}>
             Close

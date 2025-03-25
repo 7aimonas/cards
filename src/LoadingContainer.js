0d8d255e6776f5ee3608d0d6df7f39bd0c62f056
Laimonas <laimonaszakas@gmail.com>
2025-03-25 17:47:08 +0100

    loading animation added

---
+++ src/LoadingContainer.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import "./LoadingContainer.css";
+
+function LoadingContainer() {
+
+    return(
+        <div className="container">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    );
+}
+
+
+export default LoadingContainer;
